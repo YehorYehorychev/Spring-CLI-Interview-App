56e3c23f755fac86ad2d7343775c2780c681eaed
Yehor Yehorychev <egor57899@gmail.com>
2025-05-05 18:13:09 -0700

    Spring Boot bean creation demo has been refactored

--- src/main/java/com/yehorychev/interviewapp/InterviewApplication.java
+++ src/main/java/com/yehorychev/interviewapp/InterviewApplication.java
@@ -4,13 +4,24 @@ import com.yehorychev.interviewapp.service.DemoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+/* IoC (Inversion of Control) in Spring means that the control of object creation and dependency management
+ is given to the Spring container instead of the programmer.
+ Spring creates, configures, and injects beans automatically based on annotations or XML configuration.
+
+ IoC (Inversion of Control) in Spring means that objects don't create their own dependencies.
+ Instead, the IoC container (like ApplicationContext) injects the required dependencies
+ into components at creation time. This reverses the traditional flow of control and
+ simplifies configuration and testing.*/
 @SpringBootApplication
 public class InterviewApplication implements CommandLineRunner {
     private final DemoService demoService;
+    private final ApplicationContext applicationContext;
 
-    public InterviewApplication(DemoService demoService) {
+    public InterviewApplication(DemoService demoService, ApplicationContext applicationContext) {
         this.demoService = demoService;
+        this.applicationContext = applicationContext;
     }
 
     public static void main(String[] args) {
@@ -20,7 +31,13 @@ public class InterviewApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("================== " +
-                InterviewApplication.class.getName() + " is running ==================");
+                applicationContext.getId() + " is running ==================");
         demoService.processData();
+
+        DemoService demoServiceBean = applicationContext.getBean("demoService", DemoService.class);
+
+//        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+//        Arrays.stream(beanDefinitionNames)
+//                .forEach(System.out::println);
     }
 }
