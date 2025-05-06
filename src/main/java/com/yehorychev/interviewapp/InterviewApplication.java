3d42344f090e717aff5f3bb7653952aee2ea5db6
Yehor Yehorychev <egor57899@gmail.com>
2025-05-05 17:42:20 -0700

    Spring Boot bean has been created as a demo of functionality

--- src/main/java/com/yehorychev/interviewapp/InterviewApplication.java
+++ src/main/java/com/yehorychev/interviewapp/InterviewApplication.java
@@ -1,13 +1,26 @@
 package com.yehorychev.interviewapp;
 
+import com.yehorychev.interviewapp.service.DemoService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterviewApplication {
+public class InterviewApplication implements CommandLineRunner {
+    private final DemoService demoService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewApplication.class, args);
-	}
+    public InterviewApplication(DemoService demoService) {
+        this.demoService = demoService;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("================== " +
+                InterviewApplication.class.getName() + " is running ==================");
+        demoService.processData();
+    }
 }
