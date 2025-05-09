e78a5e100db875bb890f6ca201dee8d223acf6f4
Yehor Yehorychev <egor57899@gmail.com>
2025-05-08 18:15:32 -0700

    Lombok has been implemented to the Project

--- src/main/java/com/yehorychev/interviewapp/InterviewQuestionsApp.java
+++ src/main/java/com/yehorychev/interviewapp/InterviewQuestionsApp.java
@@ -1,17 +1,15 @@
 package com.yehorychev.interviewapp;
 
-import com.yehorychev.interviewapp.cli.cli.CommandLineInterface;
+import com.yehorychev.interviewapp.cli.CommandLineInterface;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class InterviewQuestionsApp implements CommandLineRunner {
     private final CommandLineInterface commandLineInterface;
-
-    public InterviewQuestionsApp(CommandLineInterface commandLineInterface) {
-        this.commandLineInterface = commandLineInterface;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(InterviewQuestionsApp.class, args);
--- src/main/java/com/yehorychev/interviewapp/controller/controller/InterviewQuestionController.java
+++ src/main/java/com/yehorychev/interviewapp/controller/controller/InterviewQuestionController.java
@@ -3,18 +3,16 @@ package com.yehorychev.interviewapp.controller.controller;
 import com.yehorychev.interviewapp.dto.dto.InterviewQuestionDto;
 import com.yehorychev.interviewapp.model.model.InterviewQuestion;
 import com.yehorychev.interviewapp.service.service.InterviewQuestionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class InterviewQuestionController {
 
     private final InterviewQuestionService service;
-
-    public InterviewQuestionController(InterviewQuestionService service) {
-        this.service = service;
-    }
 
     public InterviewQuestion createInterviewQuestion(InterviewQuestionDto questionDto) {
         return service.create(questionDto);
